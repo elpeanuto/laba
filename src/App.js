cf767ed363a521d6dbb5e0d499acf9fa5883bd2f
elpeanuto <elpeanrop@gmail.com>
2023-06-09 17:17:14 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [albums, setAlbums] = useState([]);
+  const [selectedAlbum, setAlbum] = useState(null);
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const albumResponse = await fetch('https://jsonplaceholder.typicode.com/albums');
+        const albumData = await albumResponse.json();
+        setAlbums(albumData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const fetchPhotos = async (albumId) => {
+    try {
+      const photoResponse = await fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos`);
+      const photoData = await photoResponse.json();
+      setPhotos(photoData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleAlbumClick = (album) => {
+    setAlbum(album);
+    fetchPhotos(album.id);
+  };
+
+  const handleClosePopup = () => {
+    setAlbum(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1 className="header">My Albums</h1>
+        <div className="album-grid">
+          {albums.map((album) => (
+              <div
+                  key={album.id}
+                  className="album-card"
+                  onClick={() => handleAlbumClick(album)}
+              >
+                {album.title}
+              </div>
+          ))}
+        </div>
+        {selectedAlbum && (
+            <div className="popup">
+              <h2>{selectedAlbum.title}</h2>
+              <div className="photo-grid">
+                {photos.map((photo) => (
+                    <img
+                        key={photo.id}
+                        className="photo"
+                        src={photo.url}
+                        alt={photo.title}
+                    />
+                ))}
+              </div>
+              <button className="close-button" onClick={handleClosePopup}>
+                Close
+              </button>
+            </div>
+        )}
+      </div>
   );
 }
 
